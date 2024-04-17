aded2a1ca83a50ca02c520143fbd00a38dd65716
oscariyopiyo <oscar.villarroelq@sansano.usm.cl>
2024-04-17 19:58:47 -0400

    se agrega servicio home (pagina principal)

---
+++ src/routes/health/home.js
@@ -0,0 +1,4 @@
+
+export default function getHome(ctx) {
+    ctx.body = { message: 'HOME' }
+}
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,12 @@
 import Router from 'koa-router'
 import getHealth from './health/health'
+import getHome from './health/home'
 import users from './user/user'
 
 const router = new Router()
 
+router.get('/', getHome)
 router.get('/health', getHealth)
-
 router.get('/api/users', users.getAllUsers)
 router.put('/api/user', users.createUser)
 router.delete('/api/user/:rol', users.removeUser)
